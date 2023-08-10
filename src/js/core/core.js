0990c0ea7e1720fc845ec8ac11c6a67ddc82bb39
Taegwan Youn <tkyoun0421@naver.com>
2023-08-10 13:06:31 +0900

    Docs: README.md 삭제

--- src/js/core/core.js
+++ src/js/core/core.js
@@ -16,13 +16,22 @@ function routeRender(routes) {
     }
 
     const routerView = document.querySelector("router-view");
-    const [hash, queryString = ""] = location.hash.split("?")[0];
+    const [hash, queryString = ""] = location.hash.split("?");
+
+    const query = queryString.split("&").reduce((acc, cur) => {
+        const [key, value] = cur.split("=");
+        acc[key] = value;
+        return acc;
+    }, {});
+    history.replaceState(query, "");
 
     const currentRoute = routes.find((route) =>
         new RegExp(`${route.path}/?$`).test(hash)
     );
     routerView.innerHTML = "";
     routerView.append(new currentRoute.component().el);
+
+    window.scrollTo(0, 0);
 }
 
 export function createRouter(routes) {
---
+++ src/js/routes/Profile.js
@@ -0,0 +1,10 @@
+import { Component } from "../core/core";
+
+export default class Profile extends Component {
+    render() {
+        const { a, b, c } = history.state;
+        this.el.innerHTML = /* html */ `
+
+        `;
+    }
+}
