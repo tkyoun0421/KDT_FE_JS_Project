03d04ab51edb4f6ab4286dc4be733f60deae46dc
Taegwan Youn <tkyoun0421@naver.com>
2023-08-16 15:43:36 +0900

    Feat: firestore 불러오기 완성

--- src/js/firebaseConfig.js
+++ src/js/firebaseConfig.js
@@ -1,22 +1,42 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDJflT56468jr91NjWcHlhWWZ1rtgYr8MQ",
-    authDomain: "kdt0-js-photo-board.firebaseapp.com",
-    projectId: "kdt0-js-photo-board",
-    storageBucket: "kdt0-js-photo-board.appspot.com",
-    messagingSenderId: "612459255298",
-    appId: "1:612459255298:web:6e7eb29bf7913cae74791b",
-    measurementId: "G-P90P4G9TPX",
+    apiKey: 'AIzaSyDBWZgf69JelbmqlPPPzj56W5yFL-Q5wZo',
+    authDomain: 'project-1-8debf.firebaseapp.com',
+    projectId: 'project-1-8debf',
+    storageBucket: 'project-1-8debf.appspot.com',
+    messagingSenderId: '386056815137',
+    appId: '1:386056815137:web:92425cb061808073ed9960',
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-export default firebaseConfig;
+const db = firebase.firestore();
+
+db.collection('profile')
+    .get()
+    .then((res) => {
+        res.forEach((doc) => {
+            const profileItemEl = document.querySelector('.profile-list-btm');
+            const div = document.createElement('div');
+            div.classList.add('profile-item');
+
+            let template = `<div class="profile-info">
+                    <input
+                        type="checkbox"
+                        id="profile-item"
+                        name="profile-item"
+                    />
+                    <span class="name">${doc.data().name}</span>
+                    <span class="rank">${doc.data().rank}</span>
+                </div>
+            `;
+            div.innerHTML = template;
+            profileItemEl.append(div);
+        });
+    });
